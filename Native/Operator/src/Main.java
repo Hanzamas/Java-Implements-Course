66eb820f6b706a71b2e4c899b1792d52144fe028
Verolynz <220441100156@student.trunojoyo.ac.id>
2024-07-13 11:13:34 +0700

    Operator

---
+++ Native/Operator/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 0;
+        a = a + 1;
+        a++;
+        a--;
+        System.out.println(a);
+        System.out.println(a++);
+        System.out.println(a--);
+        System.out.println(++a);
+        System.out.println(--a);
+    }
+}
