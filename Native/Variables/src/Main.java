a86f73b446c4f57c063e8162e5d385931a41dd0d
Verolynz <220441100156@student.trunojoyo.ac.id>
2024-07-13 11:07:07 +0700

    Variables

---
+++ Native/Variables/src/Main.java
@@ -0,0 +1,25 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(2+3);
+
+        int a = 2;
+        int b = 3;
+        double c = 2.3;
+        double d = 3.2;
+        String e = "Hello World";
+        String f = "!!!";
+        String g = e + f;
+        boolean h = true;
+        boolean i = false;
+
+        System.out.println(a + b);
+        System.out.println(c + d);
+        System.out.println(e + f);
+        System.out.println(g);
+        System.out.println(h);
+        System.out.println(i);
+    }
+}
