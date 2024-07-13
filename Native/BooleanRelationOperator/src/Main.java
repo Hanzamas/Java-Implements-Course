956c9715cbecaa34a740f1656dca2537ffd9289a
Verolynz <220441100156@student.trunojoyo.ac.id>
2024-07-13 12:09:09 +0700

    Relation and Boolean Operators

---
+++ Native/BooleanRelationOperator/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(1 < 2);
+        System.out.println(1 == 2);
+        System.out.println(1 != 2);
+        System.out.println(2 > 1);
+        System.out.println(2 >=2);
+        System.out.println(2 <= 2);
+        System.out.println(!true);
+        System.out.println(!false);
+        System.out.println(true && false);
+        System.out.println(true || false);
+        System.out.println(true || true);
+        System.out.println(true || false);
+        System.out.println(true && !false);
+        System.out.println(true && !true);
+        System.out.println(true || !false);
+        System.out.println(true ^ true);
+        System.out.println(true ^ false);
+    }
+}
